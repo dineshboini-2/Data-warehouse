f829f5c171e77745e6dfa637252876927044b73c
Dinesh <dineshboini2003@gmail.com>
2025-11-28 22:26:47 +0530

    Update exploartion_of_database.sql

--- scripts/Analysis on gold/exploartion_of_database.sql
+++ scripts/Analysis on gold/exploartion_of_database.sql
@@ -1,1 +1,101 @@
+---Database exploration
+-- Retrieve a list of all tables in the database
+SELECT 
+    TABLE_CATALOG, 
+    TABLE_SCHEMA, 
+    TABLE_NAME, 
+    TABLE_TYPE
+FROM INFORMATION_SCHEMA.TABLES;
+
+-- Retrieve all columns for a specific table (dim_customers)
+SELECT 
+    COLUMN_NAME, 
+    DATA_TYPE, 
+    IS_NULLABLE, 
+    CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_customers';
+
+-----------------------------------------------------------------------
+
+-----Dimentions exploration
+
+-- Retrieve a list of unique countries from which customers originate
+SELECT DISTINCT 
+    country 
+FROM gold.dim_customers
+ORDER BY country;
+
+-- Retrieve a list of unique categories, subcategories, and products
+SELECT DISTINCT 
+    category, 
+    subcategory, 
+    product_name 
+FROM gold.dim_products
+ORDER BY category, subcategory, product_name;
+
+--------------------------------------------------------------------------
+
+---date range exploartion
+
+-- Determine the first and last order date and the total duration in months
+SELECT 
+    MIN(order_date) AS first_order_date,
+    MAX(order_date) AS last_order_date,
+    DATEDIFF(MONTH, MIN(order_date), MAX(order_date)) AS order_range_months
+FROM gold.fact_sales;
+
+-- Find the youngest and oldest customer based on birthdate
+SELECT
+    MIN(birthdate) AS oldest_birthdate,
+    DATEDIFF(YEAR, MIN(birthdate), GETDATE()) AS oldest_age,
+    MAX(birthdate) AS youngest_birthdate,
+    DATEDIFF(YEAR, MAX(birthdate), GETDATE()) AS youngest_age
+FROM gold.dim_customers;
+
+
+------------------------------------------------------------------------------
+---measures exploartion 
+---like that can be aggregated
+
+-- Find the Total Sales
+SELECT SUM(sales_amount) AS total_sales FROM gold.fact_sales
+
+-- Find how many items are sold
+SELECT SUM(quantity) AS total_quantity FROM gold.fact_sales
+
+-- Find the average selling price
+SELECT AVG(price) AS avg_price FROM gold.fact_sales
+
+-- Find the Total number of Orders
+SELECT COUNT(order_number) AS total_orders FROM gold.fact_sales
+SELECT COUNT(DISTINCT order_number) AS total_orders FROM gold.fact_sales
+
+-- Find the total number of products
+SELECT COUNT(product_name) AS total_products FROM gold.dim_products
+
+-- Find the total number of customers
+SELECT COUNT(customer_key) AS total_customers FROM gold.dim_customers;
+
+-- Find the total number of customers that has placed an order
+SELECT COUNT(DISTINCT customer_key) AS total_customers FROM gold.fact_sales;
+
+-- Generate a Report that shows all key metrics of the business
+SELECT 'Total Sales' AS measure_name, SUM(sales_amount) AS measure_value FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Quantity', SUM(quantity) FROM gold.fact_sales
+UNION ALL
+SELECT 'Average Price', AVG(price) FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Orders', COUNT(DISTINCT order_number) FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Products', COUNT(DISTINCT product_name) FROM gold.dim_products
+UNION ALL
+SELECT 'Total Customers', COUNT(customer_key) FROM gold.dim_customers;
+
+--------------------------------------------------------------------------------
+
+
+
+
 
